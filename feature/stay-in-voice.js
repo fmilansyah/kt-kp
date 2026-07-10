3ef353dec7f52a5e02ed7b76a5051a30ef032f97
Febri Milansyah <febrimilan00@gmail.com>
2026-07-10 13:13:26 +0700

    Undo voice audio

--- feature/stay-in-voice.js
+++ feature/stay-in-voice.js
@@ -1,10 +1,6 @@
 const {
   joinVoiceChannel,
-  createAudioPlayer,
-  createAudioResource,
-  AudioPlayerStatus,
   VoiceConnectionStatus,
-  entersState,
 } = require("@discordjs/voice");
 
 async function joinVC(channel) {
@@ -12,37 +8,9 @@ async function joinVC(channel) {
     channelId: channel.id,
     guildId: channel.guild.id,
     adapterCreator: channel.guild.voiceAdapterCreator,
-    selfDeaf: false,
+    selfMute: true,
+    selfDeaf: true,
   });
-
-  await entersState(
-    connection,
-    VoiceConnectionStatus.Ready,
-    30000
-  );
-
-  console.log("Voice Ready");
-
-  const player = createAudioPlayer();
-
-  const resource = createAudioResource("../assets/audio/walls-ice-cream.mp3");
-  console.log(resource);
-  console.log(resource.playbackDuration);
-  connection.subscribe(player);
-
-  player.play(resource);
-
-  player.on(AudioPlayerStatus.Playing, () => {
-    console.log("Sedang memutar...");
-  });
-
-  player.on(AudioPlayerStatus.Idle, () => {
-    console.log("Lagu selesai.");
-
-    connection.destroy();
-  });
-
-  player.on("error", console.error);
 
   connection.on(VoiceConnectionStatus.Disconnected, async () => {
     setTimeout(() => {
